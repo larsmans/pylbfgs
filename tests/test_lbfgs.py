bd35db08e878bb39c0617322976d0bdf01d5ac92
Lars Buitinck <L.J.Buitinck@uva.nl>
2012-05-18 15:17:29 +0200

    TST expand tests

--- tests/test_lbfgs.py
+++ tests/test_lbfgs.py
@@ -1,6 +1,7 @@
+from nose.tools import assert_raises
 from numpy.testing import assert_array_equal
 
-from lbfgs import fmin_lbfgs
+from lbfgs import LBFGS, LBFGSError, fmin_lbfgs
 
 
 def test_fmin_lbfgs():
@@ -10,3 +11,17 @@ def test_fmin_lbfgs():
 
     xmin = fmin_lbfgs(f, 100.)
     assert_array_equal(xmin, [0])
+
+
+def test_class_interface():
+    def f(x, g, *args):
+        g[:] =  4 * x
+        return x ** 4 + 1
+
+    opt = LBFGS()
+    opt.max_iterations = 3
+
+    assert_array_equal(opt.minimize(f, 1e6), [1])
+
+    opt.max_iterations = 1
+    assert_raises(LBFGSError, opt.minimize, f, 1e7)
